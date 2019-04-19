11183b9e014b110d1b00d67e4a449219307ca62a
james <jmosul@gmail.com>
2019-04-19 19:43:19 +0100

    feat: home hero

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <v-app light>
-    <v-toolbar class="white">
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-    </v-toolbar>
     <v-content>
       <section>
-        <v-parallax src="assets/hero.jpeg" height="600">
-          <v-layout
-            column
-            align-center
-            justify-center
-            class="white--text"
-          >
-            <img src="assets/vuetify.png" alt="Vuetify.js" height="200">
-            <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
-            <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
-            <v-btn
-              class="blue lighten-2 mt-5"
-              dark
-              large
-              href="/pre-made-themes"
-            >
-              Get Started
-            </v-btn>
-          </v-layout>
-        </v-parallax>
+        <home-hero></home-hero>
       </section>
-
+      <v-toolbar class="white">
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+      </v-toolbar>
       <section>
         <v-layout
           column
@@ -186,13 +166,16 @@
 
 <script>
 
+import HomeHero from './components/HomeHero';
+
 export default {
   name: 'App',
   components: {
+      HomeHero
   },
   data () {
     return {
-      //
+
     }
   }
 }
---
+++ src/components/HomeHero.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home-hero">
+    <v-carousel hide-delimiters height="600">
+      <v-carousel-item
+        v-for="(item,i) in carouselItems"
+        :key="i"
+        :src="item.src"
+      >
+      </v-carousel-item>
+    </v-carousel>
+    <div class="hero__content">
+      <v-avatar size="168">
+        <img
+          class="elevation-20"
+          src="images/profile.jpeg"
+          alt="John"
+        >
+      </v-avatar>
+      <h1 class="display-1 white--text mt-3">James O'Sullivan</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHero",
+        data: () => ({
+            carouselItems: [
+                {
+                    src: 'images/home-carousel/P1030091-compressor.jpg'
+                },
+                {
+                    src: 'images/home-carousel/P1030311-compressor.jpg'
+                },
+                {
+                    src: 'images/home-carousel/P1030332-compressor.jpg'
+                },
+                {
+                    src: 'images/home-carousel/P1050080-compressor.jpg'
+                },
+                {
+                    src: 'images/home-carousel/P1050543-compressor.jpg'
+                }
+            ]
+        })
+    }
+</script>
+
+<style scoped>
+  .home-hero > .hero__content {
+    position: absolute;
+    top: 200px;
+    width: 100%;
+    text-align: center;
+  }
+
+  .hero__content h1 {
+    text-shadow: 3px 3px 2px rgba(0, 0, 0, 0.8);
+  }
+</style>
