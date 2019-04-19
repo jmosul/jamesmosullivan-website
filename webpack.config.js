94987410370c3e00008093714f599316952cb8ac
james <jmosul@gmail.com>
2019-04-19 16:51:31 +0100

    chore: initial

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+
+console.log( '__dirname', __dirname );
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname),
+    entry: {
+        app: './src/main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ]
+}
